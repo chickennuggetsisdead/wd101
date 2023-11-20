3638b5f8a5a0c76086443ae25b07fa76c9df16c5
Aryaman Gajrani <151563074+aryamang19@users.noreply.github.com>
2023-11-21 00:07:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,83 @@
-document.addEventListener("DOMContentLoaded", function () {
-    loadTableEntries();
-});
+//CLEAR THE DATA IN TABLE
+const dobInput = document.getElementById("dob");
+dobInput.addEventListener("input", () => validate(dobInput.value));
 
-function loadTableEntries() {
-    let usersDetails = localStorage.getItem("users");
+function validate(dobValue) {
+    const today = new Date();
+    const dobDate = new Date(dobValue);
 
-    if (usersDetails) {
-        let storedData = JSON.parse(usersDetails);
-        let tableBody = document.getElementById("tableBody");
-        tableBody.innerHTML = ""; // Clear the existing entries
+    const age = today.getFullYear() - dobDate.getFullYear();
 
-        for (let key in storedData) {
-            if (key !== "count") {
-                let user = storedData[key];
-                tableBody.innerHTML += `<tr>
-                    <td>${user.name}</td>
-                    <td>${user.email}</td>
-                    <td>${user.password}</td>
-                    <td>${user.dob}</td>
-                    <td>${user.terms}</td>
-                </tr>`;
-            }
-        }
+    if (age < 18 || age > 55) {
+        dobInput.setCustomValidity("You must be between 18 and 55 years old to register.");
+        dobInput.reportValidity();
+    } else {
+        dobInput.setCustomValidity("");
     }
 }
 
-function showError(message) {
-    let errorContainer = document.querySelector(".error-msg");
-    errorContainer.textContent = "";
-    errorContainer.textContent = message;
-}
+let userForm = document.getElementById('user-form');
 
-document.getElementById("formData").addEventListener("submit", function (event) {
+const retrieveEntries = () => {
+    let entries = localStorage.getItem('userEntries');
+    if (entries) {
+        entries = JSON.parse(entries);
+    } else {
+        entries = [];
+    }
+    return entries;
+};
+
+const displayEntries = () => {
+    const entries = retrieveEntries();
+
+    const tableEntries = entries.map((entry) => {
+        const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
+        const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
+        const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
+        const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
+        const t_cCell = `<td class='border px-4 py-2'>${entry.t_c}</td>`;
+        const row = `<tr>${nameCell} ${emailCell} ${passwordCell} ${dobCell} ${t_cCell}</tr>`;
+        return row;
+    }).join("\n");
+
+    const table = `<table class="table-auto w-full borde border-collapse border-gray-300"">
+        <tr>
+            <th class="px-4 py-2 border">Name</th>
+            <th class="px-4 py-2 border">Email</th>
+            <th class="px-4 py-2 border">Password</th>
+            <th class="px-4 py-2 border">Dob</th>
+            <th class="px-4 py-2 border">Accepted Terms?</th>
+        </tr>
+        ${tableEntries}
+    </table>`;
+
+    let details = document.getElementById('user-entries');
+    details.innerHTML = table;
+};
+
+const saveUserForm = (event) => {
     event.preventDefault();
-
-    let userName = document.getElementById("name").value;
-    let userEmail = document.getElementById("email").value;
-    let userPassword = document.getElementById("password").value;
-    let userDob = document.getElementById("dob").value;
-    let acceptedTerms = document.getElementById("agree").checked;
-
-    if (!isValidEmail(userEmail)) {
-        showError("Invalid email address");
-        return;
-    }
-
-    if (!isValidAge(userDob)) {
-        showError("Age must be between 18 and 55");
-        return;
-    }
-
-    showError("");
-
-    // Save user data to local storage
-    let users = JSON.parse(localStorage.getItem("users")) || { count: 0 };
-    let userKeyName = "user" + ++users.count;
-    let user = {
-        name: userName,
-        email: userEmail,
-        password: userPassword,
-        dob: userDob,
-        terms: acceptedTerms,
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    const dob = document.getElementById('dob').value;
+    const t_c = document.getElementById('t&c').checked;
+    
+    const entry = {
+        name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        t_c: t_c
     };
-    users
+    userEntries = retrieveEntries();
+
+    userEntries.push(entry); // Add entry to userEntries array
+
+    localStorage.setItem('userEntries', JSON.stringify(userEntries));
+    displayEntries();
+};
+
+userForm.addEventListener('submit', saveUserForm);
+displayEntries();
